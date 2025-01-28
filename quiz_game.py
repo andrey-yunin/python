411736391cee9a955fc65ed5b97d5cc9b2462a7e
andrey-yunin <andrey.yunin@gmail.com>
2025-01-28 14:40:51 +0300

    inicial commit

---
+++ quiz_game.py
@@ -0,0 +1,22 @@
+def new_game():
+    pass
+
+def check_answer():
+    pass
+
+def display_score():
+    pass
+
+def play_again():
+    pass
+
+
+questions = {"Who created Python ": "A", 
+             "What years was Python created ": "B",
+             "Python was trebuted which comedy group? ": "C",
+             "Is the Earth round? ": "A"}
+
+options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zukerburg"],
+           ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
+           ["A. Lonly Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+           ["A. True", "B. False", "C. Sometimes", "D. What is Earth?"]]
