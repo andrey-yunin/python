357d3b510fbe1fa37ec8113a61b78d0b0d91d66d
andrey-yunin <andrey.yunin@gmail.com>
2025-02-07 15:57:12 +0300

    inicial commit

--- quiz_game.py
+++ quiz_game.py
@@ -1,18 +1,58 @@
 def new_game():
-    pass
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
 
-def check_answer():
-    pass
+    for key in questions:
+        print("-----------------------------")
+        print(key)
+        for i in options[question_num-1]:
+            print(i)
+        guess = input("Enter A, B, C or D: ")
+        guess = guess.upper()
+        guesses.append(guess)
+        check_answer(questions.get(key), guess)
+        correct_guesses  += check_answer(questions.get(key), guess)
+        question_num += 1
+    
+    display_score(correct_guesses, guesses)
 
-def display_score():
-    pass
+def check_answer(answer, guess):
+    if answer == guess:
+        print("CORRECT!")
+        return 1
+    else:
+        print("WRONG!")
+        return 0
+
+def display_score(correct_guesses, guesses):
+    print ("RESULTS: ")
+    print ("Answers: ", end = " ")
+    for i in questions:
+        print(questions.get(i), end = " ")
+    print()
+
+    print ("Guesses: ", end = " ")
+    for i in guesses:
+        print(i, end = " ")
+    print()
+
+    score = int((correct_guesses/len(questions))*100)
+    print ("Your score is ", str(score), " %")
 
 def play_again():
-    pass
+    response = input("Do you want to play again? (y/n) ")
+    response = response.upper()
+
+    if response == "Y":
+        return True
+    else:
+        return False
 
 
-questions = {"Who created Python ": "A", 
-             "What years was Python created ": "B",
+
+questions = {"Who created Python? ": "A", 
+             "What years was Python created? ": "B",
              "Python was trebuted which comedy group? ": "C",
              "Is the Earth round? ": "A"}
 
@@ -20,3 +60,13 @@ options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuk
            ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
            ["A. Lonly Island", "B. Smosh", "C. Monty Python", "D. SNL"],
            ["A. True", "B. False", "C. Sometimes", "D. What is Earth?"]]
+
+new_game()
+
+while play_again():
+    new_game()
+
+print("Byeeeeeee!!!!")
+
+
+#print (options[0][0][0])
