c65336b10102488cb226b024793a7f04d3c1060c
andrey-yunin <andrey.yunin@gmail.com>
2025-02-10 14:33:55 +0300

    inicial commit

---
+++ OOP/inheritance/multiple_level_inheritance_2.py
@@ -0,0 +1,24 @@
+# multiple-level inheritance = when a derived (child) is derived from more than one parent  class
+
+class Prey:
+
+    def flee(self):
+        print("This animal flees")
+
+class Predator:
+
+    def hunt(self):
+        print("This animal is hunting")
+
+class Fish(Prey, Predator):
+    pass
+     
+
+fish = Fish()
+fish.flee()
+fish.hunt()
+
+
+
+
+
---
+++ OOP/overwriting_methods.py
@@ -0,0 +1,17 @@
+class Animal:
+
+    def eat(self):
+        print("This animal eating")
+
+class Predator(Animal):
+
+    def eat(self):
+        print("This animal is eating a rabbit")
+
+preator = Predator()
+preator.eat()
+
+
+
+
+
